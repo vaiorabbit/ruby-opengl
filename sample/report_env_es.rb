ab1b7b1caa5f45185a690a39abcd7d701126c64f
vaiorabbit <vaiorabbit+github@gmail.com>
2013-11-02 22:50:49 +0900

    sample/report_env_es.rb : Added NULL check for window handle.

--- sample/report_env_es.rb
+++ sample/report_env_es.rb
@@ -1,3 +1,4 @@
+require 'pp'
 require 'opengl_es'
 require 'glfw'
 
@@ -13,10 +14,12 @@ if __FILE__ == $0
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
   glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API)
   window = glfwCreateWindow( 1, 1, "Report OpenGL Environment", nil, nil )
-  glfwMakeContextCurrent( window )
-  puts "Version: #{glGetString(GL_VERSION).to_s}"
-  puts "Extensions:"
-  puts glGetString(GL_EXTENSIONS).to_s.split(/ /)
-  glfwDestroyWindow( window )
+  unless window.null?
+    glfwMakeContextCurrent( window )
+    puts "Version: #{glGetString(GL_VERSION).to_s}"
+    puts "Extensions:"
+    puts glGetString(GL_EXTENSIONS).to_s.split(/ /)
+    glfwDestroyWindow( window )
+  end
   glfwTerminate()
 end
