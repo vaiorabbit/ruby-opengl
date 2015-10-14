acbd771e2eb7a8ba50dac460e9cb8b02fba36d02
vaiorabbit <uk@Macmini-UK.local>
2015-10-14 23:49:15 +0900

    lib/opengl.rb: Now tries to find C Edition.

--- lib/opengl.rb
+++ lib/opengl.rb
@@ -1,7 +1,11 @@
-require_relative 'opengl_platform'
-require_relative 'opengl_common'
-require_relative 'opengl_enum'
-require_relative 'opengl_command'
-if OpenGL.get_platform == :OPENGL_PLATFORM_WINDOWS
-  require_relative 'opengl_windows'
+begin
+  require 'opengl_c'
+rescue LoadError
+  require_relative 'opengl_platform'
+  require_relative 'opengl_common'
+  require_relative 'opengl_enum'
+  require_relative 'opengl_command'
+  if OpenGL.get_platform == :OPENGL_PLATFORM_WINDOWS
+    require_relative 'opengl_windows'
+  end
 end
