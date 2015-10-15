f4c1fda7a0f5035b6b7b0ecb2a1e8b69baa7b017
vaiorabbit <uk@Macmini-UK.local>
2015-10-15 22:19:44 +0900

    sample/glfw31_build_dylib.sh: Now uses GLFW 3.1.2.

--- sample/glfw31_build_dylib.sh
+++ sample/glfw31_build_dylib.sh
@@ -3,12 +3,12 @@
 #
 # Ref.: https://github.com/malkia/ufo/blob/master/build/OSX/glfw.sh
 #
-wget http://downloads.sourceforge.net/project/glfw/glfw/3.1.1/glfw-3.1.1.tar.bz2
-tar xvjf glfw-3.1.1.tar.bz2
-cd glfw-3.1.1/
+wget http://downloads.sourceforge.net/project/glfw/glfw/3.1.2/glfw-3.1.2.tar.bz2
+tar xvjf glfw-3.1.2.tar.bz2
+cd glfw-3.1.2/
 mkdir build
 cd build
-export MACOSX_DEPLOYMENT_TARGET=10.10
+export MACOSX_DEPLOYMENT_TARGET=10.11
 cmake -D GLFW_NATIVE_API=1 -D CMAKE_OSX_ARCHITECTURES="i386;x86_64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
 make
 
