104c9a119d134e32664d8f9fd09595a83be9eb84
vaiorabbit <vaiorabbit+github@gmail.com>
2022-04-10 15:24:21 +0900

    Updated GLFW-related scripts

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ...and wrapper code generator.
 
 *   Created : 2013-08-28
-*   Last modified : 2022-03-27
+*   Last modified : 2022-04-10
 
 ---
 
--- sample/glfw_build.sh
+++ sample/glfw_build.sh
@@ -1,13 +1,14 @@
 #
-# For Mac OS X + Xcode + CMake users.
+# For macOS + Xcode + CMake users.
 #
 # Ref.: https://github.com/malkia/ufo/blob/master/build/OSX/glfw.sh
 #
-git clone --depth=1 https://github.com/glfw/glfw.git glfw_master
-cd glfw_master/
+curl -O -L https://github.com/glfw/glfw/releases/download/3.3.7/glfw-3.3.7.zip
+tar xvjf glfw-3.3.7.zip
+cd glfw-3.3.7/
 mkdir build
 cd build
 export MACOSX_DEPLOYMENT_TARGET=10.14
-cmake -D CMAKE_BUILD_TYPE=Release -D GLFW_NATIVE_API=1 -D CMAKE_OSX_ARCHITECTURES="x86_64;arm64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
+cmake -D CMAKE_BUILD_TYPE=Release -D GLFW_NATIVE_API=1 -D CMAKE_OSX_ARCHITECTURES="arm64;x86_64" -D BUILD_SHARED_LIBS=ON -D CMAKE_C_COMPILER=clang ../
 make
 cp -R src/libglfw* ../..
