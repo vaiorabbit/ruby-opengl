fdca87859ff77f42192114bb4103dcd51965aee3
vaiorabbit <vaiorabbit+github@gmail.com>
2022-04-10 15:13:51 +0900

    Updated GLFW-related scripts

--- sample/glfw_build.sh
+++ sample/glfw_build.sh
@@ -3,9 +3,9 @@
 #
 # Ref.: https://github.com/malkia/ufo/blob/master/build/OSX/glfw.sh
 #
-curl -O -L https://github.com/glfw/glfw/releases/download/3.3.6/glfw-3.3.6.zip
-tar xvjf glfw-3.3.6.zip
-cd glfw-3.3.6/
+curl -O -L https://github.com/glfw/glfw/releases/download/3.3.7/glfw-3.3.7.zip
+tar xvjf glfw-3.3.7.zip
+cd glfw-3.3.7/
 mkdir build
 cd build
 export MACOSX_DEPLOYMENT_TARGET=10.14
--- sample/glfw_get.sh
+++ sample/glfw_get.sh
@@ -1,3 +1,3 @@
-curl -O -L https://github.com/glfw/glfw/releases/download/3.3.6/glfw-3.3.6.bin.MACOS.zip
-tar -xf glfw-3.3.6.bin.MACOS.zip
-cp glfw-3.3.6.bin.MACOS/lib-universal/libglfw.3.dylib ./libglfw.dylib
+curl -O -L https://github.com/glfw/glfw/releases/download/3.3.7/glfw-3.3.7.bin.MACOS.zip
+tar -xf glfw-3.3.7.bin.MACOS.zip
+cp glfw-3.3.7.bin.MACOS/lib-universal/libglfw.3.dylib ./libglfw.dylib
